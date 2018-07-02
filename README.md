2d5ce9f3cd8d944bb5dccfea38ace0d2f218cd94
PUGE <100284685@qq.com>
2018-07-02 18:28:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# notice-puge
+
+![default](http://p5qgrn52w.bkt.clouddn.com/%E5%BD%95%E5%88%B6_2018_07_02_18_24_37_54.gif)
+
+安装
+```
+npm i -save notice-puge
+或
+yarn add notice-puge
+```
+
+## 参数
+
+| 参数        | 含义         | 类型  | 是否必须  |
+| ----------- |:-------------:| -----:| -----:|
+|text| 显示的文字 | String | true |
---
+++ notice.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="notice-puge-box">
+    <div class="item" :style="{top: '0'}">{{copyValue}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  },
+  watch: {
+    text (newValue) {
+      const para = document.createElement("div")
+      para.classList.add('item')
+      para.style.top = '100%'
+      para.innerHTML = newValue
+      this.$el.appendChild(para)
+      setTimeout(() => {
+        this.$el.children[0].style.top = '-100px'
+        this.$el.children[1].style.top = '0'
+        setTimeout(() => {
+          this.$el.removeChild(this.$el.children[0])
+        }, 1000)
+      }, 0)
+    }
+  },
+  data () {
+    return {
+      copyValue: this.text
+    }
+  }
+}
+</script>
+
+<style>
+  .notice-puge-box {
+    height: 89px;
+    margin: 0 40px;
+    margin-top: 20px;
+    line-height: 90px;
+    color: #6c6c6c;
+    overflow: hidden;
+    font-size: 1.4rem;
+    position: relative;
+    width: calc(100% - 380px);
+    border-bottom: 1px solid #dddddd;
+  }
+  .notice-puge-box .item {
+    width: 100%;
+    position: absolute;
+    transition: all 1s;
+  }
+</style>
